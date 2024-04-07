1286131b81a5d16562ff1854dd10132bdf37c7a2
Aytaditya <adityaaryan531@gmail.com>
2024-04-07 11:51:53 +0530

    setup for tailwind css and daisy-ui

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar bg-primary text-primary-content">
+        <a className="btn btn-ghost font-mono text-3xl" href="/"><i class="fa-solid fa-ghost"></i> Glimpse</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
