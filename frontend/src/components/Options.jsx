74319e4e14864d4eaeb9778b3155ae1e5f892f0f
Aytaditya <adityaaryan531@gmail.com>
2024-05-12 02:13:58 +0530

    id to call field added

--- frontend/src/components/Options.jsx
+++ frontend/src/components/Options.jsx
@@ -17,11 +17,19 @@ const Options = ({ children }) => {
 
   return (
     <div className='flex justify-center rounded-2xl glass items-center pb-3 '>
-      <div className='h-[25vh]  w-[30vw] flex flex-col text-blue-300 backdrop:blur-lg'>
-        <div className='font-mono text-3xl pl-5 pt-3 font-bold'>Account</div>
-        <div className='pl-5 pt-3 flex flex-col'>
+      <div className='h-[25vh]  w-[60vw] flex flex-col text-blue-300 backdrop:blur-lg'>
+        <div className="flex flex-row justify-between">
+          <p className='font-mono text-3xl pl-5 pt-3 font-bold'>Account</p>
+          <p className='font-mono text-2xl pr-[100px] pt-3 font-bold'>Make a Call</p>
+        </div>
+        <div className='pl-5 pt-3 flex flex-row gap-14'>
           <input type="text" placeholder="Name" className="input input-bordered input-info w-full input-sm max-w-xs  " value={name} onChange={(e)=>{setName(e.target.value)}} id="area" />
-          <button className="btn btn-success w-20 btn-sm mt-3" onClick={handleCopy} >Copy <i class="fa-solid fa-copy"></i></button>
+          <input type="text" placeholder="ID to Call" className="input input-bordered input-info w-full input-sm max-w-xs" />
+          
+        </div>
+        <div className='flex flex-row gap-14 justify-between items-center'>
+          <button className="btn btn-primary w-[100px] btn-sm ml-[100px] mt-4" onClick={handleCopy} >Copy <i class="fa-solid fa-copy"></i></button>
+          <button className="btn btn-success  w-[100px] btn-sm mr-[150px] mt-4" >Call <i class="fa-solid fa-phone"></i></button>
         </div>
         
       </div>
